06a3668b9a56f423cc5f56a538c73cacf92ae021
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2023-04-20 09:45:22 -0500

    feat(setup): install and configure eslint, airbnb style guide and prettier (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
   return (
-    <>
-      <p className="read-the-docs">
-        Welcome to the Memory Game
-      </p>
-    </>
-  )
+    <p className="read-the-docs">
+      Welcome to the Memory Game
+    </p>
+  );
 }
 
-export default App
+export default App;
