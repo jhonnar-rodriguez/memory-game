2eb4719c3df37cb264c9e8744dcb3ce4fd177f1d
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2023-04-20 09:35:29 -0500

    chore(eslint-setup): new settings were added to implement eslint with the airbnb style guide

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,17 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', project: './tsconfig.json', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
