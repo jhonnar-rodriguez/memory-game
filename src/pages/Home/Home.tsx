2bb05b2df9daedf74c80e184c526891316471809
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2023-04-20 23:25:48 -0500

    feat(instructions-modal): new instructions modal wired up with the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,37 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import {
+  Button,
   Col,
   Container,
   Row,
 } from 'react-bootstrap';
 
+import { Instructions } from '../../components/Instructions';
+
 export function Home(): ReactElement {
+  const [displayInstructionsModal, setDisplayInstructionsModal] = useState<boolean>(false);
+
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center mt-5">
+    <Container className="d-flex flex-column justify-content-center align-items-center">
       <Row>
         <Col className="text-center">
-          <h1>Home Page</h1>
+          <h1>Welcome to the Memory Game</h1>
+          <p className="my-4">Test your memory by finding all the matching pairs!</p>
+          <div className="d-grid gap-2">
+            <Button size="lg" variant="primary">
+              Start Game
+            </Button>
+            <Button onClick={() => setDisplayInstructionsModal(true)} size="lg" variant="secondary">
+              See Instructions
+            </Button>
+          </div>
         </Col>
       </Row>
+
+      <Instructions
+        onHideModal={() => setDisplayInstructionsModal(false)}
+        show={displayInstructionsModal}
+      />
     </Container>
   );
 }
