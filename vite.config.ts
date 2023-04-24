46f973d50c32f558efeb971251654473f5e9db1e
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2023-04-24 17:56:44 -0500

    ci(deployment): adding workflow to deploy the site to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/memory-game/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
